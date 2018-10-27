aca19c85dc35268b483c4ce7b7c1f9cb9093bf25
Samit Koyom <samitkoyom@gmail.com>
2018-10-27 10:05:15 +0700

    เริ่มต้นบันทึกประวัติครั้งแรก

---
+++ src/app/app.module.ts
@@ -0,0 +1,65 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
+import { MyApp } from './app.component';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+// Side menu page
+import { SideSchedulePage } from '../pages/side-schedule/side-schedule';
+import { SidePortfolioPage } from '../pages/side-portfolio/side-portfolio';
+import { SidePaymentPage } from '../pages/side-payment/side-payment';
+import { SideSettingPage } from '../pages/side-setting/side-setting';
+
+// Tab menu page
+import { TabHomePage } from '../pages/tab-home/tab-home';
+import { TabCoursePage } from '../pages/tab-course/tab-course';
+import { TabServicePage } from '../pages/tab-service/tab-service';
+import { TabArticlePage } from '../pages/tab-article/tab-article';
+import { TabContactPage } from '../pages/tab-contact/tab-contact';
+import { TabsPage } from '../pages/tabs/tabs';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    // Side menu
+    SideSchedulePage,
+    SidePortfolioPage,
+    SidePaymentPage,
+    SideSettingPage,
+    // Tab menu
+    TabHomePage,
+    TabCoursePage,
+    TabServicePage,
+    TabArticlePage,
+    TabContactPage,
+    TabsPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+     // Side menu
+     SideSchedulePage,
+     SidePortfolioPage,
+     SidePaymentPage,
+     SideSettingPage,
+     // Tab menu
+     TabHomePage,
+     TabCoursePage,
+     TabServicePage,
+     TabArticlePage,
+     TabContactPage,
+     TabsPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
